520b13304073fb11319c7d05e625a79daff1b4b1
thenewl <thenewl@users.noreply.github.com>
2024-03-16 11:48:17 +0200

    f

--- scripts/cicd/test_go/start_docker_compose.sh
+++ scripts/cicd/test_go/start_docker_compose.sh
@@ -5,5 +5,7 @@ if ! command -v yq &> /dev/null; then
     wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY} -O /usr/bin/yq && chmod +x /usr/bin/yq
 fi
 
+echo Image Template: ${CONTAINER_REGISTRY}/${GIT_FULL_REPO_NAME}/<service>:${DOCKER_IMAGE_TAG}
+
 # Boot up services
 docker compose up -d
