5fe74f8bc303b1011ba0d301193ac516ef063479
thenewl <thenewl@users.noreply.github.com>
2024-03-16 11:22:53 +0200

    try fixing

--- scripts/cicd/publish_npm_package/publish_npm_package.sh
+++ scripts/cicd/publish_npm_package/publish_npm_package.sh
@@ -1,8 +1,7 @@
 set -e
-(
-  ./scripts/apply_env.sh ./src/packages/varsoapi/package.json
-  cd src/packages/varsoapi
-  npm config set registry https://npm.pkg.github.com/
-  npm unpublish @varsotech/varsoapi@${GIT_REF} || echo No version to unpublish
-  npm publish
-)
+
+./scripts/apply_env.sh ./src/packages/varsoapi/package.json
+cd src/packages/varsoapi
+npm config set registry https://npm.pkg.github.com/
+npm unpublish @varsotech/varsoapi@${GIT_REF} || echo No version to unpublish
+npm publish
