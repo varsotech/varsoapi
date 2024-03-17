8102a8c109a29ba534f6cb975d8c31fdf13d1636
thenewl <thenewl@users.noreply.github.com>
2024-03-17 22:20:54 +0200

    react app

--- src/services/www/components/Input/Input.style.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components"
-import { Theme } from "../Theme/Theme"
-
-export const Input = styled.input<{ theme: Theme }>`
-  border: 1px solid ${({ theme }) => theme.colors.neutral.main};
-  border-radius: 10px;
-  font-size: ${({ theme }) => theme.font.medium}px;
-  padding: 5px 10px;
-`
