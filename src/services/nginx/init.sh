56a0011d72d7149ca1c34ab544004ef4933ff440
thenewl <thenewl@users.noreply.github.com>
2024-03-25 21:05:59 +0200

    fix certificate bug

--- src/services/nginx/init.sh
+++ src/services/nginx/init.sh
@@ -4,12 +4,12 @@ certdir="/etc/letsencrypt/live"
 realcert="${certdir}/.realcert"
 fakecert="${certdir}/.fakecert"
 
-# Only create dummy certificate if no cert exists
-if [ ! -f "$path/fullchain.pem" ]; then
+# Only create dummy certificate if .realcert doesn't exist
+if [ ! -f "$realcert" ]; then
     for domain in ${CERTBOT_DOMAINS}; do
-        path="/etc/letsencrypt/live/${domain}"
-        mkdir -p "$path"
-        openssl req -x509 -nodes -newkey rsa:4096 -days 1 -keyout "$path/privkey.pem" -out "$path/fullchain.pem" -subj "/CN=localhost"
+        certPath="${certdir}/${domain}"
+        mkdir -p "$certPath"
+        openssl req -x509 -nodes -newkey rsa:4096 -days 1 -keyout "$certPath/privkey.pem" -out "$certPath/fullchain.pem" -subj "/CN=localhost"
     done
 
     mkdir -p "${certdir}"
