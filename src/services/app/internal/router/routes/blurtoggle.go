c6824a1564df3d5a86ab55d8a9818f28522a7e44
thenewl <thenewl@users.noreply.github.com>
2024-03-25 10:03:43 +0200

    f

--- src/services/app/internal/router/routes/blurtoggle.go
+++ src/services/app/internal/router/routes/blurtoggle.go
@@ -8,9 +8,14 @@ import (
 	"github.com/varsotech/varsoapi/src/common/api"
 	"github.com/varsotech/varsoapi/src/services/app/client/models"
 	"github.com/varsotech/varsoapi/src/services/app/internal/modules/rssitem"
+	"github.com/varsotech/varsoapi/src/services/auth/internal/modules/permission"
 )
 
 func BlurToggle(w *api.Writer, r *http.Request, p httprouter.Params, j *api.JWT, request models.BlurToggleRequest) (*any, *api.Error) {
+	if !j.Permissions[permission.KeyNewsItemImageBlur] {
+		return nil, api.NewUnauthorizedError(nil, "no permission to toggle blur")
+	}
+
 	err := rssitem.ToggleBlur(r.Context(), uuid.MustParse(request.RssItemId))
 	if err != nil {
 		return nil, api.NewInternalError(err, "failed toggling blur")
